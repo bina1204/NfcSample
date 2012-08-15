cd98810cb58de11eaccdfdea1c43a764a36735e3
bina1204 <6OTiau!9>
2012-08-15 15:06:51 +0900

    NFC Sample.

---
+++ src/com/gsbina/android/nfc/sample/NfcTagViewerActivity.java
@@ -0,0 +1,20 @@
+package com.gsbina.android.nfc.sample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NfcTagViewerActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_tag_viewer);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_nfc_tag_viewer, menu);
+        return true;
+    }
+}
